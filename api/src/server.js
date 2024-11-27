7061c80f6b8f85c95352e96a63040c74cd57763d
Dimitri-G11 <122949286+Dimitri-G11@users.noreply.github.com>
2024-11-27 14:56:57 -0300

    first commit

---
+++ api/src/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express=require("express");
+const routes =require("./routes.js")
+const connectToDatabase =require("./database")
+
+connectToDatabase();
+
+const app =express();
+const port = 3333;
+
+app.use(routes)
+app.listen(port,()=>{console.log(`Backend started at http://localhost:${port}`)});
